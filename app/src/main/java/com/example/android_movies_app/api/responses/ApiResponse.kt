6da514fabbbc28f1fd34d23a23aa4f876a959870
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-07-11 20:54:54 +0300

    main

--- app/src/main/java/com/example/android_movies_app/api/responses/ApiResponse.kt
+++ app/src/main/java/com/example/android_movies_app/api/responses/ApiResponse.kt
@@ -1,4 +1,16 @@
 package com.example.android_movies_app.api.responses
 
-class ApiResponse {
+import retrofit2.Response
+
+sealed class ApiResponse<T> {
+    companion object{
+        fun <T> create(error: Throwable): ApiResponse<T>{
+            return ApiErrorResponse(error.message ?: "Unknown Error, Check Network Connection")
+        }
+
+
+
+
+    }
+
 }
