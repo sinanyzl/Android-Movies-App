750525441f197e14ba1494705ff86e481659e965
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-07-11 21:16:04 +0300

    main

--- app/src/main/java/com/example/android_movies_app/api/responses/ApiResponse.kt
+++ app/src/main/java/com/example/android_movies_app/api/responses/ApiResponse.kt
@@ -9,8 +9,29 @@ sealed class ApiResponse<T> {
         }
 
 
-
-
+        fun <T> create(response: Response<T>): ApiResponse<T>{
+            return if (response.isSuccessful){
+                val body = response.body()
+                if (body == null || response.code() == 204){
+                    ApiEmptyResponse()
+                }else{
+                    ApiSuccessResponse(
+                        body = body
+                    )
+                }
+            }else {
+                val msg = response.errorBody()?.string()
+                val errorMsg = if (msg.isNullOrEmpty()) {
+                    response.message()
+                }else {
+                    msg
+                }
+                ApiErrorResponse(errorMsg ?: "Error, Check Network Connect!")
+            }
+        }
     }
-
 }
+
+class ApiEmptyResponse<T> : ApiResponse<T>()
+data class ApiSuccessResponse<T>(val body: T): ApiResponse<T>()
+data class ApiErrorResponse<T>(val errorMessage: String): ApiResponse<T>()
