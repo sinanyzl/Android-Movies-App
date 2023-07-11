a70a8676d42f6685393fe1811493f47965fdd40b
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-07-11 20:11:48 +0300

    Initial commit

---
+++ app/src/main/java/com/example/android_movies_app/api/responses/inner/CreditsResponse.kt
@@ -0,0 +1,4 @@
+package com.example.android_movies_app.api.responses.inner
+
+class CreditsResponse {
+}
---
+++ app/src/main/java/com/example/android_movies_app/persistence/dao/MovieAndDetailDao.kt
@@ -0,0 +1,4 @@
+package com.example.android_movies_app.persistence.dao
+
+class MovieAndDetailDao {
+}
