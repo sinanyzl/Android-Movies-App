852146f5ca1042941eca9c18d1d4de7f83b29c80
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-07-16 19:20:46 +0300

    main

--- app/src/main/java/com/example/android_movies_app/repository/MovieDetailRepository.kt
+++ app/src/main/java/com/example/android_movies_app/repository/MovieDetailRepository.kt
@@ -1,4 +1,13 @@
 package com.example.android_movies_app.repository
 
-class MovieDetailRepository {
+import androidx.lifecycle.LiveData
+import com.bumptech.glide.load.engine.Resource
+import com.example.android_movies_app.api.MoviesApi
+import com.example.android_movies_app.models.MovieDetails
+
+class MovieDetailRepository (private val movieApi: MoviesApi) {
+
+    fun getMovieDetail(movieId: Long): LiveData<Resource<MovieDetails>> {
+        return object :
+    }
 }
