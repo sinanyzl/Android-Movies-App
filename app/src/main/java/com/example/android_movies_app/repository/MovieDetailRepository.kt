4a4f897c5d649606b3406f3cc410af68c0ece0fe
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-07-19 23:18:05 +0300

    master

--- app/src/main/java/com/example/android_movies_app/repository/MovieDetailRepository.kt
+++ app/src/main/java/com/example/android_movies_app/repository/MovieDetailRepository.kt
@@ -6,31 +6,36 @@ import com.example.android_movies_app.api.MoviesApi
 import com.example.android_movies_app.api.responses.ApiEmptyResponse
 import com.example.android_movies_app.api.responses.ApiErrorResponse
 import com.example.android_movies_app.api.responses.ApiResponse
+import com.example.android_movies_app.api.responses.ApiSuccessResponse
 import com.example.android_movies_app.api.responses.MovieDetailsResponse
 import com.example.android_movies_app.models.MovieDetails
 import java.util.ResourceBundle
 
-class MovieDetailRepository (private val movieApi: MoviesApi) {
+class MovieDetailRepository( private val movieApi: MoviesApi) {
+
 
     fun getMovieDetail(movieId: Long): LiveData<Resource<MovieDetails>> {
-        return object : handleApiSuccessResponse(response: ApiSuccessResponse<MovieDetailsResponse>){
-            val detailsResponse = response.body
-            val videos = detailsResponse.videoResponse?.videos
-            val casts = detailsResponse.creditsResponse?.casts
-            val reviews = detailsResponse.reviewResponse?.reviews
-            result.value = Resource.Success(MovieDetails(videos, reviews, casts))
-        }
+        return object : NetworkBoundResourceNoCaching<MovieDetails, MovieDetailsResponse>() {
+            override fun handleApiSuccessResponse(response: ApiSuccessResponse<MovieDetailsResponse>) {
+                val detailsResponse= response.body
+                val videos = detailsResponse.videoResponse?.videos
+                val casts = detailsResponse.creditsResponse?.casts
+                val reviews = detailsResponse.reviewResponse?.reviews
+                result.value = Resource.Success(MovieDetails(videos, reviews, casts))
+            }
 
-        override fun handleApiEmptyResponse(response: ApiEmptyResponse<MovieDetailsResponse>){
-            result.value = Resource.Success(MovieDetails())
-        }
+            override fun handleApiEmptyResponse(response: ApiEmptyResponse<MovieDetailsResponse>) {
+                //MovieDetails,has empty videos,casts,reviews
+                result.value = Resource.Success(MovieDetails())
+            }
 
-        override fun handleApiErrorResponse(response: ApiErrorResponse<MovieDetailsResponse>){
-            result.value = Resource.Error(response.errorMessage, null)
-        }
+            override fun handleApiErrorResponse(response: ApiErrorResponse<MovieDetailsResponse>) {
+                result.value = Resource.Error(response.errorMessage,null)
+            }
 
-        override fun createCall(): LiveData<ApiResponse<MovieDetailsResponse>> = movieApi.getMovieDetail(moveId)
+            override fun createCall(): LiveData<ApiResponse<MovieDetailsResponse>> =
+                movieApi.getMovieDetail(movieId)
 
-
-    }.asLiveData()
+        }.asLiveData()
+    }
 }
--- app/src/main/java/com/example/android_movies_app/repository/MoviesRepository.kt
+++ app/src/main/java/com/example/android_movies_app/repository/MoviesRepository.kt
@@ -1,4 +1,49 @@
 package com.example.android_movies_app.repository
 
-class MoviesRepository {
+import android.util.Log
+import androidx.lifecycle.LiveData
+import com.example.android_movies_app.api.MoviesApi
+import com.example.android_movies_app.api.responses.ApiResponse
+import com.example.android_movies_app.api.responses.MoviesResponse
+import com.example.android_movies_app.persistence.dao.MovieAndDetailDao
+import com.example.android_movies_app.util.AppExecutors
+import com.example.android_movies_app.util.Category
+import com.example.android_movies_app.util.NetworkBoundResource
+import com.example.android_movies_app.util.Resource
+import com.example.android_movies_app.model.Movie
+import com.example.android_movies_app.models.Movie
+import com.example.android_movies_app.responses.MoviesResponse
+import java.util.Locale.Category
+
+
+
+class MoviesRepository (
+        private val movieDao: MovieAndDetailDao,
+        private val appExecutors: AppExecutors,
+        private val movieApi: MoviesApi
+        ){
+        companion object{ private const val TAG = "MoviesRepository"}
+
+        fun getListMovie(pageNumber: Int, category: Category): LiveData<Resource<List<Movie>>> {
+                return object : NetworkBoundResource<List<Movie>, MoviesResponse>(appExecutors) {
+                        item?.let{
+                                val list: ArrayList<Move>? = (item.movies)?.let {ArrayList(it)}
+                                val newList: ArrayList<Movie>? = ArrayList<Movie>()
+                                list?.forEach {
+                                        val movie = it.copy(categoryType = category)
+                                        Log.d(TAG, "saveCallResult: ${movie}")
+                                        newList?.add(movie)
+                                }
+                                newList?.let { movieDao.insertMovies(*newList.toTypedArray()) }
+                        }
+                }
+
+                override fun shouldFetch(data: List<Movie>?): Boolean {
+                        return true
+                }
+
+
+
+
+        }
 }
