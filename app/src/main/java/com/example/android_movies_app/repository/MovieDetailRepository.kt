fe5a59a860214647bbce9f87e115d132ad3b8ea8
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-07-19 22:55:48 +0300

    master

--- app/src/main/java/com/example/android_movies_app/repository/MovieDetailRepository.kt
+++ app/src/main/java/com/example/android_movies_app/repository/MovieDetailRepository.kt
@@ -3,11 +3,34 @@ package com.example.android_movies_app.repository
 import androidx.lifecycle.LiveData
 import com.bumptech.glide.load.engine.Resource
 import com.example.android_movies_app.api.MoviesApi
+import com.example.android_movies_app.api.responses.ApiEmptyResponse
+import com.example.android_movies_app.api.responses.ApiErrorResponse
+import com.example.android_movies_app.api.responses.ApiResponse
+import com.example.android_movies_app.api.responses.MovieDetailsResponse
 import com.example.android_movies_app.models.MovieDetails
+import java.util.ResourceBundle
 
 class MovieDetailRepository (private val movieApi: MoviesApi) {
 
     fun getMovieDetail(movieId: Long): LiveData<Resource<MovieDetails>> {
-        return object :
-    }
+        return object : handleApiSuccessResponse(response: ApiSuccessResponse<MovieDetailsResponse>){
+            val detailsResponse = response.body
+            val videos = detailsResponse.videoResponse?.videos
+            val casts = detailsResponse.creditsResponse?.casts
+            val reviews = detailsResponse.reviewResponse?.reviews
+            result.value = Resource.Success(MovieDetails(videos, reviews, casts))
+        }
+
+        override fun handleApiEmptyResponse(response: ApiEmptyResponse<MovieDetailsResponse>){
+            result.value = Resource.Success(MovieDetails())
+        }
+
+        override fun handleApiErrorResponse(response: ApiErrorResponse<MovieDetailsResponse>){
+            result.value = Resource.Error(response.errorMessage, null)
+        }
+
+        override fun createCall(): LiveData<ApiResponse<MovieDetailsResponse>> = movieApi.getMovieDetail(moveId)
+
+
+    }.asLiveData()
 }
