d38a696ee8978efb958cf1b99e8162ba30e7537e
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-07-19 23:32:35 +0300

    master

--- app/src/main/java/com/example/android_movies_app/repository/MoviesRepository.kt
+++ app/src/main/java/com/example/android_movies_app/repository/MoviesRepository.kt
@@ -25,16 +25,21 @@ class MoviesRepository (
         companion object{ private const val TAG = "MoviesRepository"}
 
         fun getListMovie(pageNumber: Int, category: Category): LiveData<Resource<List<Movie>>> {
+
                 return object : NetworkBoundResource<List<Movie>, MoviesResponse>(appExecutors) {
-                        item?.let{
-                                val list: ArrayList<Move>? = (item.movies)?.let {ArrayList(it)}
-                                val newList: ArrayList<Movie>? = ArrayList<Movie>()
-                                list?.forEach {
-                                        val movie = it.copy(categoryType = category)
-                                        Log.d(TAG, "saveCallResult: ${movie}")
-                                        newList?.add(movie)
+
+                        override fun savedCallResult(item: MoviesResponse?){
+
+                                item?.let{
+                                        val list: ArrayList<Move>? = (item.movies)?.let {ArrayList(it)}
+                                        val newList: ArrayList<Movie>? = ArrayList<Movie>()
+                                        list?.forEach {
+                                                val movie = it.copy(categoryType = category)
+                                                Log.d(TAG, "saveCallResult: ${movie}")
+                                                newList?.add(movie)
+                                        }
+                                        newList?.let { movieDao.insertMovies(*newList.toTypedArray()) }
                                 }
-                                newList?.let { movieDao.insertMovies(*newList.toTypedArray()) }
                         }
                 }
 
@@ -42,8 +47,35 @@ class MoviesRepository (
                         return true
                 }
 
+                override fun loadFromDb(): LiveData<List<Movie>> = movieDao.getMovies(pageNumber, category)
 
 
+                override fun createCall(): LiveData<ApiResponse<MoviesResponse>> = when (category) {
+                        Category.TOPRATED -> movieApi.getTopRatedMovies(pageNumber)
+                        Category.UPCOMING -> movieApi.getUpcomingMovies(pageNumber)
+                        else -> movieApi.getPopulerMovies(pageNumber)
+                }
 
-        }
+        }.asLiveData()
+
+
+        fun searchListMovie(pageNumber: Int, query: String): LiveData<Resource<List<Movie>>> {
+                return object : NetworkBoundResource<List<Movie>, MoviesResponse>(appExecutors){
+
+                        override fun saveCallResult(item: MoviesResponse?) {
+                                item?.let {
+                                        val list: ArrayList<Movie>? = (item.movies)?.let { ArrayList(it) }
+                                        list?.let { movieDao.insertMovies(*list.toTypedArray()) }
+                                }
+                        }
+                }
+
+                override fun shouldFetch(data: List<Movie>?) : Boolean = true
+
+                override fun loadFromDb(): LiveData<List<Movie>> = movieDao.searchListMovie(query, pageNumber)
+
+                override fun createCall(): LiveData<ApiResponse<MoviesResponse>> = movieApi.searchMovies(query, pageNumber)
+
+        }.asLiveData()
+
 }
