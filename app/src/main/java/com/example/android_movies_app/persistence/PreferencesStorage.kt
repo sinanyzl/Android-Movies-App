e211dc34571fcf8d668bcec87e7c93ea516fcad9
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-07-21 21:37:15 +0300

    master

--- app/src/main/java/com/example/android_movies_app/persistence/PreferencesStorage.kt
+++ app/src/main/java/com/example/android_movies_app/persistence/PreferencesStorage.kt
@@ -1,6 +1,7 @@
 package com.example.android_movies_app.persistence
 
 import android.content.Context
+import com.example.android_movies_app.util.Category
 
 
 private const val PREF_CATEGORY = "pref_category"
@@ -10,6 +11,32 @@ object PreferencesStorage {
 
     fun getStoredQuery(context: Context): String? {
         val prefs = androidx.preference.PreferenceManager.getDefaultSharedPreference(context)
+        returm prefs.getString(PREF_QUERY, null)
+    }
+
+    fun setStoredQuery(context: Context, value: String?){
+        androidx.preference.PreferenceManager.getDefaultSharedPreference(context)
+            .edit()
+            .putString(PREF_QUERY, value)
+            .apply()
+    }
+
+    fun getStoredCategory(context: Context): Category{
+        val prefs = androidx.preference.PreferenceManager.getDefaultSharedPreference(context)
+        val valueString = prefs.getString(PREF_CATEGORY, Category.POPULAR.toString())
+        return when(valueString){
+            Category.TOPRATED.toString() -> Category.TOPRATED
+            Category.UPCOMING.toString() -> Category.UPCOMING
+            else -> Category.POPULAR
+        }
+    }
+
+    fun setStoredCategory(context: Context, value: Category){
+        val valueString = value.toString()
+        androidx.preference.PreferenceManager.getDefaultSharedPreference(context)
+            .edit()
+            .putString(PREF_CATEGORY, valueString)
+            .apply()
     }
 
 }
