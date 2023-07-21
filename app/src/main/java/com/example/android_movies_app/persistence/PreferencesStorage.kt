3cf81e1c4e849602a0d244fd0a6754b1d414293a
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-07-21 21:29:41 +0300

    master

--- app/src/main/java/com/example/android_movies_app/persistence/PreferencesStorage.kt
+++ app/src/main/java/com/example/android_movies_app/persistence/PreferencesStorage.kt
@@ -1,4 +1,15 @@
 package com.example.android_movies_app.persistence
 
-class PreferencesStorage {
+import android.content.Context
+
+
+private const val PREF_CATEGORY = "pref_category"
+private const val PREF_QUERY = "pref_query"
+
+object PreferencesStorage {
+
+    fun getStoredQuery(context: Context): String? {
+        val prefs = androidx.preference.PreferenceManager.getDefaultSharedPreference(context)
+    }
+
 }
---
+++ app/src/main/java/com/example/android_movies_app/util/Category.kt
@@ -0,0 +1,4 @@
+package com.example.android_movies_app.util
+
+class Category {
+}
