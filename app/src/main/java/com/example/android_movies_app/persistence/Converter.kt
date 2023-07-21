45724bfaca58ed91b27535bc5065b082f19a7f08
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-07-21 20:59:41 +0300

    master

--- app/src/main/java/com/example/android_movies_app/persistence/Converter.kt
+++ app/src/main/java/com/example/android_movies_app/persistence/Converter.kt
@@ -1,4 +1,26 @@
 package com.example.android_movies_app.persistence
 
+
+import androidx.room.TypeConverter
+
+import com.example.android_movies_app.movies.util.Category
+
 class Converter {
+
+    @TypeConverter
+    fun catToInt(cat: Category?): Int?{
+        return cat?.ordinal
+    }
+
+    @TypeConverter
+    fun intToCat(int: Int): Category? {
+        return int?.let {
+            when(it){
+                0 -> Category.POPULAR
+                1 -> Category.UPCOMING
+                2 -> Category.TOPRATED
+                else -> Category.POPULAR
+            }
+        }
+    }
 }
