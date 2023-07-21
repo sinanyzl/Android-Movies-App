ae39a569ce696d39b7df5d1b851c78ed15222c62
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-07-21 21:12:18 +0300

    master

--- app/src/main/java/com/example/android_movies_app/persistence/MovieDB.kt
+++ app/src/main/java/com/example/android_movies_app/persistence/MovieDB.kt
@@ -1,4 +1,20 @@
 package com.example.android_movies_app.persistence
 
-class MovieDB {
+import androidx.room.Database
+import androidx.room.TypeConverters
+import com.example.android_movies_app.persistence.dao.*
+import com.example.android_movies_app.models.Cast
+import com.example.android_movies_app.models.Movie
+import com.example.android_movies_app.models.Review
+import com.android.myapplication.popularmovies.api.model.Video
+
+@Database(
+    entities = [Movie::class],
+    version = 1,
+    exportSchema = true
+)
+
+@TypeConverters(Converter:: class)
+abstract class MovieDB : Database(){
+    abstract val movieDao: MovieAndDetailDao
 }
