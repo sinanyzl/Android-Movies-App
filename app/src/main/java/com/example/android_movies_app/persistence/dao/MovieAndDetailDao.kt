c1359350ce5a86f147dd8ef2c32f0a2f28234829
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-07-21 20:53:44 +0300

    master

--- app/src/main/java/com/example/android_movies_app/persistence/dao/MovieAndDetailDao.kt
+++ app/src/main/java/com/example/android_movies_app/persistence/dao/MovieAndDetailDao.kt
@@ -1,4 +1,28 @@
 package com.example.android_movies_app.persistence.dao
 
-class MovieAndDetailDao {
+import androidx.lifecycle.LiveData
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import androidx.room.Update
+import com.example.android_movies_app.models.Movie
+
+@Dao
+interface MovieAndDetailDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    fun insertMovies(vararg movie: Movie):LongArray
+
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    fun insertMovie(movie: Movie)
+
+    @Update
+    fun updateMovies(movie: Movie)
+
+    @Query("SELECT * FROM movie WHERE category=:category LIMIT (:pageNumber*20)")
+    fun getMovies(pageNumber: Int, category): LiveData<List<Movie>>
+
+    @Query("SELECT * FROM movie WHERE title LIKE '%' || :query || LIMIT (:pageNumber*20)")
+    fun searchListMovie(query: String, pageNumber: Int): LiveData<List<Movie>>
+
 }
