9ae373c0aa8dc4dcbd1aa99bd56a4d5bb666430a
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-07-26 19:35:42 +0300

    master

--- app/src/main/java/com/example/android_movies_app/models/Movie.kt
+++ app/src/main/java/com/example/android_movies_app/models/Movie.kt
@@ -1,7 +1,11 @@
 package com.example.android_movies_app.models
 
+import com.example.android_movies_app.util.Category
+import android.os.Parcelable
+import androidx.room.ColumnInfo
 import androidx.room.Entity
 import androidx.room.Index
+import com.google.gson.annotations.SerializedName
 import kotlinx.android.parcel.Parcelize
 
 @Parcelize
@@ -10,5 +14,44 @@ import kotlinx.android.parcel.Parcelize
 )
 
 data class Movie (
+    @SerializedName("id")
+    val id: Long = 0,
 
-        )
+    @SerializedName("title")
+    val title: String? = null,
+
+    @ColumnInfo(name = "poster_path")
+    @SerializedName("poster_path")
+    val posterPath: String? = null,
+
+
+    @ColumnInfo(name = "backdrop_path")
+    @SerializedName("backdrop_path")
+    val backdropPath: String? = null,
+
+    @ColumnInfo(name = "vote_average")
+    @SerializedName("vote_average")
+    val voteAverage: Double? = 0.0,
+
+    @ColumnInfo(name = "vote_count")
+    @SerializedName("vote_count")
+    val voteCount: Int = 0,
+
+    @SerializedName("original_language")
+    val language: String? = null,
+
+    @ColumnInfo(name = "release_date")
+    @SerializedName("release_date")
+    val releaseDate: String? = null,
+
+    @SerializedName("overview")
+    val overview: String? = null,
+
+    @SerializedName("popularity")
+    val popularity: Double = 0.0,
+
+    @ColumnInfo(name = "category")
+    val categoryType: Category = Category.POPULAR
+
+
+):Parcelable{ }
