c8c031d8856642958ba45b880e8536f21abce163
Sinan Yozgatlı <sinanyozgatli01@gmail.com>
2023-07-31 22:30:55 +0300

    master

--- app/src/main/java/com/example/android_movies_app/util/NetworkBoundResourceNoCaching.kt
+++ app/src/main/java/com/example/android_movies_app/util/NetworkBoundResourceNoCaching.kt
@@ -1,4 +1,53 @@
 package com.example.android_movies_app.util
 
-class NetworkBoundResourceNoCaching {
+import androidx.annotation.MainThread
+import androidx.annotation.WorkerThread
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MediatorLiveData
+import com.example.android_movies_app.api.responses.ApiEmptyResponse
+import com.example.android_movies_app.api.responses.ApiErrorResponse
+import com.example.android_movies_app.api.responses.ApiResponse
+import com.example.android_movies_app.api.responses.ApiSuccessResponse
+
+
+@Suppress("LeakingThis")
+abstract class NetworkBoundResourceNoCaching<UiObject, RequestObject> @MainThread constructor() {
+
+
+    protected val result = MediatorLiveData<Resource<UiObject>>()
+
+
+    init {
+        result.value = Resource.Loading(null)
+        val apiResponse = createCall()
+        result.addSource(apiResponse) { response ->
+            result.removeSource(apiResponse)
+            handleNetworkCall(response)
+        }
+    }
+
+
+    fun handleNetworkCall(response: ApiResponse<RequestObject>) {
+        when (response) {
+            is ApiSuccessResponse -> {
+                handleApiSuccessResponse(response)
+            }
+            is ApiEmptyResponse -> {
+                handleApiEmptyResponse(response)
+            }
+            is ApiErrorResponse -> {
+                handleApiErrorResponse(response)
+            }
+        }
+    }
+
+    fun asLiveData() = result as LiveData<Resource<UiObject>>
+
+    abstract fun handleApiSuccessResponse(response: ApiSuccessResponse<RequestObject>)
+    abstract fun handleApiEmptyResponse(response: ApiEmptyResponse<RequestObject>)
+    abstract fun handleApiErrorResponse(response: ApiErrorResponse<RequestObject>)
+
+    @MainThread
+    protected abstract fun createCall(): LiveData<ApiResponse<RequestObject>>
+
 }
